f81d963a4f0056cdec6f44e6ee73d8a0dca42ca1
kevxxf <kevin.fosse057@gmail.com>
2019-01-03 19:38:34 +0100

    Re-Write
    New version from blank page

--- main.cpp
+++ main.cpp
@@ -1,61 +1,73 @@
 //
 //  main.cpp
-//  coursdemath
+//  new-delta
 //
-//  Created by Kevin Fosse on 19/11/2018.
-//  Copyright © 2018 Kevin Fosse. All rights reserved.
-//
+//  Created by Kevin Fosse on 03/01/2019.
+//  Copyright © 2019 Kevin Fosse. All rights reserved.
+// Version non fini
 
 #include <iostream>
-#include <string>
-void calcMath() {
-    double anumber = 0;
-    double bnumber = 0;
-    double cnumber = 0;
-    double result = 0;
-    std::string userYes;
-    
-    
-    
 
-    std::cout << "Veuillez entrez A" << std::endl;
-    std::cin >> anumber;
-    std::cout << "Veuillez entrez B" << std::endl;
-    std::cin >> bnumber;
-    std::cout << "Veuillez entrez C" << std::endl;
-    std::cin >> cnumber;
-    std::cout << "Valeur A : " << anumber << "; valeur B : " << bnumber << "; valeur C : " << cnumber << std::endl;;
+struct letter {
+    double a;
+    double b;
+    double c;
+    double result;
+};
+
+int nb;
+void SaisieNumber();
+
+
+void SaisieNumber(letter & param){
+    std::cout << "Entrez la valeur de A" << std::endl;
+    std::cin >> param.a;
     
-    result = (bnumber * bnumber ) - 4 * anumber * cnumber ;
-    std::cout << "Le résultat du calcul est : " << result << std::endl;
+    std::cout << "Entrez la valeur de B" << std::endl;
+    std::cin >> param.b;
     
-    if (result < 0) {
-        std::cout << "Cette équation n'a aucune solution réelle." << std::endl;
-    }
-    else if (result == 0) {
-        std::cout << "Cette équation a seulement une solution réelle." << std::endl;
-    }
-    else {
-        std::cout << "Cette équation a deux solutions réelles." << std::endl;
-    }
-    std::cout << "Voulez vous relancer le programme ? (oui/non)" << std::endl;
-    std::cin >> userYes;
-    if(userYes == "oui" || userYes == "OUI") {
-        std::cout << "=======================================" << std::endl;
-        std::cout << " " << std::endl;
-        calcMath();
-    }
+    std::cout << "Entrez la valeur de C" << std::endl;
+    std::cin >> param.c;
     
-    else {
-        
-    }
+    
+    
     
 }
+
 int main(int argc, const char * argv[]) {
-    std::cout << "Calculateur de Delta par Kévin Fosse\n";
-    calcMath();
-   
+    // Allocation dynamique de nombre de fois
+    std::cout << "Entrez le nombre de calcul a effectué" << std::endl;
     
+    std::cin >> nb;
+    while (nb < 1) {
+        std::cout << "Erreur, le nombre de calcul ne peux pas être inférieur à 1" << std::endl;
+        std::cin >> nb;
+    }
+    
+    letter *number = new letter[nb];
+    if(number == NULL) { std::cout << "Erreur d'allocation, redemarrer le programme ! \n"; std::exit(0);}
+    else {std::cout << "Allocation Réussite ! \n";}
+     // execution de la suite de calcul
+    
+    for (int i = 0; i<nb; i++) {
+        SaisieNumber(number[i]);
+    }
+    
+    // Fin
+    system("clear");
+    std::cout << "Voici le résultat des calculs : " << std::endl;
+    for (int i = 0; i<nb; i++) {
+        double result = (number[i].b * number[i].b) - 4 * number[i].a * number[i].c ;
 
-
+        std::cout << i+1 << " |  " << "("<< number[i].b << " * " << number[i].b << ")" << " - 4 * " << number[i].a << " * " << number[i].c << " = " << result << std::endl;
+        if(result < 0){std::cout << "Cette équation n'a aucune solution réelle." << std::endl;}
+        else if (result > 0){std::cout << "Cette équation a deux solutions réelles." << std::endl;}
+        else {std::cout << "Cette équation a seulement une solution réelle." << std::endl;}
+        std::cout << "\n";
+    }
+    
+    
+    delete [] number;;
+    
+    return 0;
 }
